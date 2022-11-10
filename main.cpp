44a06c0e039b47e34d75af3ec40c9ee1e0fb976e
Nebomuk <taiko000@gmail.com>
2022-11-10 19:54:01 +0100

    added app and org name

--- main.cpp
+++ main.cpp
@@ -8,6 +8,10 @@ int main(int argc, char *argv[])
 #ifdef Q_OS_ANDROID
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
+    
+    QCoreApplication::setOrganizationName("RayTrace");
+    QCoreApplication::setOrganizationDomain("RayTrace");
+    QCoreApplication::setApplicationName("RayTrace Experiment");
     QApplication app(argc, argv);
     MainWindow window;
     window.show();
